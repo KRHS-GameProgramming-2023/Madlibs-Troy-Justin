8b16aaaa19499f6d521ae7e54ad8a4d01964c2e6
Tmadigan324 <Troy.Madigan@kearsarge.org>
2022-10-24 10:42:30 -0400

    added getState

--- Getter.py
+++ Getter.py
@@ -60,13 +60,90 @@ def getSport(promt, debug = False):
         goodInput = True
         if isSwear(word, debug):
             goodInput = False
-            print ("Don't use laguage like that")
+            print ("Don't use language like that")
         elif word.lower() not in sports:
             goodInput = False
             print("Sorry, I don't know that one.")
             
     return word
     
+def getState(promt, debug = False):
+    if debug: print ("getState Function")
+
+    goodInput = False
+#list from https://capitalizemytitle.com/50-us-states-in-alphabetical-order/
+    states = ["Alabama",
+            "Alaska",
+            "Arizona", 
+            "Arkansas",
+            "California",
+            "Colorado",
+            "Connecticut",
+            "Delaware",
+            "Florida",
+            "Georgia",
+            "Hawaii",
+            "Idaho",
+            "Illinois",
+            "Indiana",
+            "Iowa",
+            "Kansas",
+            "Kentucky",
+            "Louisiana",
+            "Maine",
+            "Maryland",
+            "Massachusetts",
+            "Michigan",
+            "Minnesota",
+            "Mississippi",
+            "Missouri",
+            "Montana",
+            "Nebraska",
+            "Nevada",
+            "New Hampshire",
+            "New Jersey",
+            "New Mexico",
+            "New York",
+            "North Carolina",
+            "North Dakota",
+            "Ohio",
+            "Oklahoma",
+            "Oregon",
+            "Pennsylvania",
+            "Rhode Island",
+            "South Carolina",
+            "South Dakota",
+            "Tennessee",
+            "Texas",
+            "Utah",
+            "Vermont",
+            "Virginia",
+            "Washington",
+            "West Virginia",
+            "Wisconsin",
+            "Wyoming"]
+    
+    while not goodInput:
+        word = input(promt)
+        
+        goodInput = True
+        if isSwear(word, debug):
+            goodInput = False
+            print ("Don't use language like that")
+        elif word.capitalize() not in states:
+            goodInput = False
+            if " " in word:
+                words=word.split(" ")
+                if len(words) == 2:
+                    word=words[0].capitalize()+" "+words[1].capitalize()
+                    if word in states:
+                        return word
+            print("Sorry, I don't know that one.")
+        return word
+            
+    
+    
+
     
 def isSwear(word, debug = False):
     if debug: print("isSwear Function")
@@ -97,4 +174,7 @@ swearList = ["poop",
              "cock sucker",
              "fucker",
 ]
-        
+   
+   
+# ~ while True:
+    # ~ getState(">")
